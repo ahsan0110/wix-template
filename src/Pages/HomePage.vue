ce83c161813d8cbc7fd6511e073c3b234289452f
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-25 20:01:14 +0500

    hero component complete with scroll image

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,13 +1,14 @@
 <template>
     <NavBar />
-
+    <HeroComponent />
 </template>
 <script>
+import HeroComponent from '../components/HeroComponent.vue';
 import NavBar from '../components/NavBar.vue';
 
 export default {
     name: 'HomePage',
-    components: { NavBar },
+    components: { NavBar, HeroComponent },
 }
 
 </script>
---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="hero-section">
+        <div class="hero-img-layer"></div>
+        <div class="section-card">
+            <h1>TAILORED <br>MARKETING <br>STRATEGIES</h1>
+            <h4>Empowering Businesses Digitally</h4>
+            <button id="hero-btn" class="btn">Explore More</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+
+
+    mounted() {
+        const layer = document.querySelector(".hero-img-layer");
+        let lastScroll = window.scrollY;
+        let offset = 0;
+
+        window.addEventListener("scroll", () => {
+            const current = window.scrollY;
+            const delta = current - lastScroll;
+
+            offset += delta * -0.5; // subtle drift
+            layer.style.transform = `translateX(${offset}px)`;
+
+            lastScroll = current;
+        });
+    }
+
+
+}
+</script>
