8e004d3ec3327e927e9dc26c0bb385b760af4a92
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-26 13:24:31 +0500

    fixed image responsive and paragraph width

--- src/components/HomeGrid.vue
+++ src/components/HomeGrid.vue
@@ -4,21 +4,17 @@
         <div class="div1">
             <h1>Our Story</h1>
             <p>T. Kensington is a top-tier digital marketing agency dedicated to providing specialized online branding
-                <br>
-                solutions and cutting-edge marketing strategies for small and mid-sized enterprises. With a team of <br>
+                solutions and cutting-edge marketing strategies for small and mid-sized enterprises. With a team of
                 innovative thinkers and analytical experts, we excel in SEO, PPC, social media campaigns, and content
-                <br>
                 marketing, ensuring impactful outcomes for our clients.
             </p>
         </div>
         <div class="div2">
             <h1>Our Mission</h1>
-            <p>At T. Kensington, we strive to transform businesses by crafting unique and data-centric marketing <br>
+            <p>At T. Kensington, we strive to transform businesses by crafting unique and data-centric marketing
                 campaigns. Our project, 'Local Thrive,' exemplifies our commitment to driving engagement and boosting
-                <br>
-                online visibility for our clients, resulting in remarkable growth and success. We take pride in our <br>
+                online visibility for our clients, resulting in remarkable growth and success. We take pride in our
                 ability to create compelling brand narratives that resonate with audiences, driving enhanced reach and
-                <br>
                 conversions.
 
             </p>
