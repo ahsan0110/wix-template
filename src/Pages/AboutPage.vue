3feae59ea8eff722d73254929c8a7095ef2325dc
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 15:43:36 +0500

    created co-worker component

--- src/Pages/AboutPage.vue
+++ src/Pages/AboutPage.vue
@@ -4,7 +4,9 @@
     <HeroComponent bgImage="/headphone.jpg" title="LOCAL <br>THRIVE <br>SUCCESS" subtitle="Standout Projects"
         :showButton="false" :showPara="true"
         para="T. Kensington is a dynamic digital agency known for its innovative marketing strategies and impactful solutions. With a dedicated team of creative thinkers and data analysts, we specialize in SEO, PPC, social media, and content marketing to enhance brand visibility and drive conversions." />
-        
+
+    <CoWorkers />
+    
     <HeroComponent bgImage="/blaclwhite.png" title="DRIVING<br>SUCCESS" subtitle="Our Mission" :showButton="false"
         :showPara="true"
         para="Our mission is to create data-driven marketing campaigns that resonate with audiences, boost online traffic, and elevate brand recognition. We aim to transform businesses with compelling storytelling and outcome-focused strategies." />
@@ -17,9 +19,10 @@ import HeroComponent from '../components/HeroComponent.vue';
 import NavBar from '../components/NavBar.vue';
 import Footer from '../components/Footer.vue'
 import BlackCard from '../components/BlackCard.vue';
+import CoWorkers from '../components/CoWorkers.vue';
 
 export default {
     name: 'AboutPage',
-    components: { NavBar, HeroComponent, Footer, BlackCard }
+    components: { NavBar, HeroComponent, Footer, BlackCard, CoWorkers }
 }
 </script>
---
+++ src/components/CoWorkers.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="co-wrokers-section">
+        <div class="co-worker-parent">
+            <div class="co-worker">
+                <img src="/twogirls.jpg" alt="">
+            </div>
+            <div class="co-worker-grey"></div>
+            <div class="co-worker-black"></div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+</script>
