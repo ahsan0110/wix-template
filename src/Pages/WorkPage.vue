854c96400ab22287e1d2791472a844a58ef993b1
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 18:07:07 +0500

    completed our work page

--- src/Pages/WorkPage.vue
+++ src/Pages/WorkPage.vue
@@ -5,6 +5,7 @@
     title="EXPLORE OUR <br>SUCCESSFUL PROJECTS"
    />
    <Portfolio/>
+   <Portfolio2/>
     <Footer/>
     
 </template>
@@ -14,10 +15,11 @@ import NavBar from '../components/NavBar.vue';
 import Footer from '../components/Footer.vue';
 import BlackCard from '../components/BlackCard.vue';
 import Portfolio from '../components/portfolio.vue';
+import Portfolio2 from '../components/Portfolio2.vue';
 
 
     export default{
         name:'WorkPage',
-        components:{ NavBar, Footer, BlackCard,Portfolio}
+        components:{ NavBar, Footer, BlackCard,Portfolio, Portfolio2}
     }
 </script>
---
+++ src/components/Portfolio2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="portfolio2-container">
+        <div class="earing">
+            <img src="/Earing.png" alt="">
+        </div>
+        <div class="guldier">
+            <img src="/guldier.png" alt="">
+        </div>
+        <div class="cream">
+            <img src="/cream.png" alt="">
+        </div>
+        <div class="flex-board">
+            <img src="/flex.png" alt="">
+        </div>
+    </div>
+</template>
