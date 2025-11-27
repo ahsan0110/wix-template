d4d9531032edbb15563cf2e6de6c95121d9eb19f
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 19:07:49 +0500

    Contact page Done

--- src/Pages/ContactPage.vue
+++ src/Pages/ContactPage.vue
@@ -1,19 +1,24 @@
 <template>
-    <NavBar/>
-    
-    <Footer/>
-    
+    <NavBar />
+    <ContactForm img-src="/purple_gradient.png" :show-overlay="true" overlay-title="GET IN TOUCH WITH US TODAY
+
+" overlay-subtitle="Contact us" />
+
+
+    <Footer />
+
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue';
 import Footer from '../components/Footer.vue'
+import ContactForm from '../components/ContactForm.vue'
 
 
-    export default{
-        name:'ContactPage',
-        components:{
-            NavBar, Footer
-        }
+export default {
+    name: 'ContactPage',
+    components: {
+        NavBar, Footer, ContactForm,
     }
+}
 </script>
--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="contact-form-section">
         <div class="contact-img">
-            <img src="/sitting_person.png" alt="">
+            <img :src="imgSrc" alt="">
+            <div v-if="showOverlay" class="img-overlay">
+                <p>{{ overlaySubtitle }}</p>
+                <h1>{{ overlayTitle }}</h1>
+            </div>
         </div>
         <div class="contact-form">
             <div class="contact-head">
@@ -35,3 +39,80 @@
         </div>
     </div>
 </template>
+<script>
+export default {
+    name: "ContactForm",
+    props: {
+        imgSrc: { type: String, default: "/sitting_person.png" },
+        showOverlay: { type: Boolean, default: false },
+        overlayTitle: { type: String, default: "" },
+        overlaySubtitle: { type: String, default: "" }
+    }
+};
+</script>
+
+<style>
+.contact-img {
+    border-radius: 20px;
+    position: relative;
+}
+
+.img-overlay {
+    position: absolute;
+    top: 10%;
+    left: 10%;
+    text-align: left;
+    color: white;
+    width: 80%;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+
+.img-overlay h1 {
+    margin-top: 32px;
+    font-size: 62px;
+}
+
+.img-overlay p {
+    font-size: clamp(14px, 1.4vw, 32px)
+}
+
+@media (max-width: 1024px) {
+    .img-overlay {
+        top: 25%;
+        left: 5%;
+        width: 70%;
+    }
+
+    .img-overlay h1 {
+        margin-top: 16px;
+        font-size: 62px;
+    }
+
+    .img-overlay p {
+        font-size: clamp(14px, 1.4vw, 32px);
+    }
+}
+
+@media (max-width: 768px) {
+    .img-overlay {
+        top: 10%;
+        left: 5%;
+        width: 90%;
+    }
+
+}
+
+@media (max-width: 480px) {
+    .img-overlay {
+        top: 15%;
+        left: 5%;
+        width: 90%;
+    }
+
+    .img-overlay h1 {
+       
+        font-size: 32px;
+    }
+
+}
+</style>
