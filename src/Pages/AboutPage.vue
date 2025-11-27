7728ad441b77e1a9c8e0d4850f6996b1096e2bc4
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 14:49:40 +0500

    Added BlackCard

--- src/Pages/AboutPage.vue
+++ src/Pages/AboutPage.vue
@@ -1,5 +1,6 @@
 <template>
     <NavBar />
+    <BlackCard />
     <HeroComponent bgImage="/headphone.jpg" title="LOCAL <br>THRIVE <br>SUCCESS" subtitle="Standout Projects"
         :showButton="false" :showPara="true"
         para="T. Kensington is a dynamic digital agency known for its innovative marketing strategies and impactful solutions. With a dedicated team of creative thinkers and data analysts, we specialize in SEO, PPC, social media, and content marketing to enhance brand visibility and drive conversions." />
@@ -11,9 +12,10 @@
 import HeroComponent from '../components/HeroComponent.vue';
 import NavBar from '../components/NavBar.vue';
 import Footer from '../components/Footer.vue'
+import BlackCard from '../components/BlackCard.vue';
 
 export default {
     name: 'AboutPage',
-    components: { NavBar, HeroComponent, Footer }
+    components: { NavBar, HeroComponent, Footer, BlackCard }
 }
 </script>
---
+++ src/components/BlackCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="black-card-section">
+        <div class="black-card">
+            <p>{{ para }}</p>
+            <h1 v-html="title"></h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "BlackCard",
+    props: {
+        para: { type: String, default: "Innovative Strategies" },
+        title: { type: String, default: "MARKETING <br>SOLUTIONS <br>EXPERTS" },
+    }
+}
+</script>
