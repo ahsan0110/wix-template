7130190843d0d08e3839834c7a382bee58dd43e1
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 16:03:08 +0500

    completed about page

--- src/Pages/AboutPage.vue
+++ src/Pages/AboutPage.vue
@@ -6,7 +6,8 @@
         para="T. Kensington is a dynamic digital agency known for its innovative marketing strategies and impactful solutions. With a dedicated team of creative thinkers and data analysts, we specialize in SEO, PPC, social media, and content marketing to enhance brand visibility and drive conversions." />
 
     <CoWorkers />
-    
+    <TwoImg />
+
     <HeroComponent bgImage="/blaclwhite.png" title="DRIVING<br>SUCCESS" subtitle="Our Mission" :showButton="false"
         :showPara="true"
         para="Our mission is to create data-driven marketing campaigns that resonate with audiences, boost online traffic, and elevate brand recognition. We aim to transform businesses with compelling storytelling and outcome-focused strategies." />
@@ -20,9 +21,10 @@ import NavBar from '../components/NavBar.vue';
 import Footer from '../components/Footer.vue'
 import BlackCard from '../components/BlackCard.vue';
 import CoWorkers from '../components/CoWorkers.vue';
+import TwoImg from '../components/TwoImg.vue';
 
 export default {
     name: 'AboutPage',
-    components: { NavBar, HeroComponent, Footer, BlackCard, CoWorkers }
+    components: { NavBar, HeroComponent, Footer, BlackCard, CoWorkers,TwoImg }
 }
 </script>
---
+++ src/components/TwoImg.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="two-img-main">
+        <div class="two-img-images">
+            <img src="/diamond.png" alt="">
+        </div>
+        <div class="two-img-images">
+            <img src="/usinglaptop.jpg" alt="">
+        </div>
+    </section>
+</template>
