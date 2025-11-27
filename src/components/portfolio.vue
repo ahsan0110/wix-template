c5d1c4223e81dacb5956e4d4149317a96d661421
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 22:45:26 +0500

    Added slider

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -1,28 +1,32 @@
 <template>
     <div class="portfolio-container">
-        <div class="black-white-1">
-            <img src="/blaclwhite.png" alt="">
-        </div>
-        <div class="headphone-2">
-            <img src="/headphone.jpg" alt="">
-        </div>
-        <div class="stains-3">
-            <img src="/stains.png" alt="">
-        </div>
-        <div class="lovely-day-5">
-            <img src="/lovely-day.png" alt="">
-        </div>
-        <div class="bill-board-6">
-            <img src="/billboard.png" alt="">
-        </div>
-        <div class="metro-4">
-            <img src="/metro.png" alt="">
-        </div>
-        <div class="glasses-7">
-            <img src="/glases.png" alt="">
-        </div>
-        <div class="watch-8">
-            <img src="/watch.png" alt="">
+        <div v-for="(item, index) in portfolioItems" :key="index" :class="item.class" @click="goToSlider(item, index)">
+            <img :src="item.src" :alt="item.alt" />
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: "Portfolio",
+    data() {
+        return {
+            portfolioItems: [
+                { class: "black-white-1", src: "/blaclwhite.png", alt: "Black & White" },
+                { class: "headphone-2", src: "/headphone.jpg", alt: "Headphone" },
+                { class: "stains-3", src: "/stains.png", alt: "Stains" },
+                { class: "lovely-day-5", src: "/lovely-day.png", alt: "Lovely Day" },
+                { class: "bill-board-6", src: "/billboard.png", alt: "Billboard" },
+                { class: "metro-4", src: "/metro.png", alt: "Metro" },
+                { class: "glasses-7", src: "/glases.png", alt: "Glasses" },
+                { class: "watch-8", src: "/watch.png", alt: "Watch" },
+            ],
+        };
+    },
+    methods: {
+        goToSlider(item, index) {
+            this.$router.push({ name: "Slider", query: { startIndex: index } });
+        },
+    },
+};
+</script>
