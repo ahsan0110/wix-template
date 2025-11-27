15f38adf65d838f4b9666942c4058e2e3162ed46
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 19:21:17 +0500

    service page hero section

--- src/Pages/ServicePage.vue
+++ src/Pages/ServicePage.vue
@@ -1,8 +1,12 @@
 <template>
-    <NavBar/>
-    
-    <Footer/>
-    
+    <NavBar />
+    <div class="service-hero">
+        <p>Our Approach</p>
+        <h1>TAILORED MARKETING <br>SOLUTIONS</h1>
+        <p>Empower your brand with our strategic expertise.</p>
+    </div>
+    <Footer />
+
 </template>
 
 <script>
@@ -10,10 +14,10 @@ import NavBar from '../components/NavBar.vue';
 import Footer from '../components/Footer.vue'
 
 
-    export default{
-        name:'ServicePage',
-        components:{
-            NavBar, Footer
-        }
+export default {
+    name: 'ServicePage',
+    components: {
+        NavBar, Footer
     }
+}
 </script>
