e116a33e66a0f4b7183a4fc78971487ea810d5bb
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-27 15:06:06 +0500

    added new component balckWhite

--- src/Pages/AboutPage.vue
+++ src/Pages/AboutPage.vue
@@ -4,6 +4,10 @@
     <HeroComponent bgImage="/headphone.jpg" title="LOCAL <br>THRIVE <br>SUCCESS" subtitle="Standout Projects"
         :showButton="false" :showPara="true"
         para="T. Kensington is a dynamic digital agency known for its innovative marketing strategies and impactful solutions. With a dedicated team of creative thinkers and data analysts, we specialize in SEO, PPC, social media, and content marketing to enhance brand visibility and drive conversions." />
+        
+    <HeroComponent bgImage="/blaclwhite.png" title="DRIVING<br>SUCCESS" subtitle="Our Mission" :showButton="false"
+        :showPara="true"
+        para="Our mission is to create data-driven marketing campaigns that resonate with audiences, boost online traffic, and elevate brand recognition. We aim to transform businesses with compelling storytelling and outcome-focused strategies." />
     <Footer />
 
 </template>
